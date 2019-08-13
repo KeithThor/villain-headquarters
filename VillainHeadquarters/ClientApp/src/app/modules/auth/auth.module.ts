efce3a26f24f41d2fbeaae9e0f1214b0fc5eaf7c
Keith Thor <keithkeethor@gmail.com>
2019-08-13 15:24:38 -0700

    Fix module import issues

--- VillainHeadquarters/ClientApp/src/app/modules/auth/auth.module.ts
+++ VillainHeadquarters/ClientApp/src/app/modules/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountService } from './services/account.service';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
@@ -18,11 +20,15 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [
     LoginComponent,
     RegisterComponent
+  ],
+  providers: [
+    AccountService
   ]
 })
 export class AuthModule { }
