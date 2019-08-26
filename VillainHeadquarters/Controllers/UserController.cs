a4e1fdd514908821c9cc237ec74d43acc36b1b0c
Keith Thor <keithkeethor@gmail.com>
2019-08-25 22:15:53 -0700

    Implement the UserController
    Implemented the UserController with login and register post methods.
    Created a RegistrationService that will interface with EntityFramework
    to create new Users.

---
+++ VillainHeadquarters/Controllers/UserController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VillainHeadquarters.Models;
+using VillainHeadquarters.Services;
+
+namespace VillainHeadquarters.Controllers
+{
+    /// <summary>
+    /// Controller responsible for logging in and registering users.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserFinder _userFinder;
+        private readonly TokenBuilder _tokenBuilder;
+        private readonly RegistrationService _registrationService;
+
+        public UserController(UserFinder userFinder,
+                              TokenBuilder tokenBuilder,
+                              RegistrationService registrationService)
+        {
+            _userFinder = userFinder;
+            _tokenBuilder = tokenBuilder;
+            _registrationService = registrationService;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login([FromBody]UserCredentials user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new
+                {
+                    Message = "Server received invalid data for login request."
+                });
+            }
+
+            var foundUser = await _userFinder.FindUserAsync(user);
+            if (foundUser == null)
+            {
+                return BadRequest(new
+                {
+                    Message = "No user was found with that username and password."
+                });
+            }
+
+            var token = _tokenBuilder.CreateToken(foundUser);
+            return new JsonResult(new {
+                Token = token,
+                Username = foundUser.UserName,
+                Id = foundUser.Id
+            });
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register([FromBody]UserCredentials user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new
+                {
+                    Message = "Server received invalid data for register request."
+                });
+            }
+
+            var newUser = await _registrationService.RegisterUserAsync(user);
+            if (newUser == null)
+            {
+                return BadRequest(new
+                {
+                    Message = "A user with that username already exists."
+                });
+            }
+
+            var token = _tokenBuilder.CreateToken(newUser);
+            return new JsonResult(new
+            {
+                Token = token,
+                Username = newUser.UserName,
+                Id = newUser.Id
+            });
+        }
+    }
+}
---
+++ VillainHeadquarters/Services/RegistrationService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using VillainHeadquarters.Data;
+using VillainHeadquarters.Models;
+
+namespace VillainHeadquarters.Services
+{
+    public class RegistrationService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistrationService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        /// Registers a new basic user account asynchronously.
+        /// </summary>
+        /// <param name="user">The credentials of the new user to register.</param>
+        /// <returns>Returns an ApplicationUser if registration was successful, else returns null.</returns>
+        public async Task<ApplicationUser> RegisterUserAsync(UserCredentials user)
+        {
+            if (await _userManager.FindByNameAsync(user.Username) != null) return null;
+
+            var id = Guid.NewGuid().ToString();
+            var claims = GetUserClaims(user, id );
+
+            var newUser = new ApplicationUser
+            {
+                Id = id,
+                UserName = user.Username,
+                Claims = claims
+            };
+
+            var result = await _userManager.CreateAsync(newUser, user.Password);
+            if (result.Succeeded) return newUser;
+            else return null;
+        }
+
+        /// <summary>
+        /// Constructs and returns a new List of IdentityUserClaims for the given user.
+        /// </summary>
+        /// <param name="user">The object that holds the user's credentials.</param>
+        /// <param name="userId">The id of the new user.</param>
+        /// <returns></returns>
+        private List<IdentityUserClaim<string>> GetUserClaims(UserCredentials user, string userId)
+        {
+            return new List<IdentityUserClaim<string>>
+            {
+                new IdentityUserClaim<string>
+                {
+                    ClaimType = ClaimTypes.Name,
+                    ClaimValue = user.Username
+                },
+                new IdentityUserClaim<string>
+                {
+                    ClaimType = ClaimTypes.NameIdentifier,
+                    ClaimValue = userId
+                },
+                new IdentityUserClaim<string>
+                {
+                    ClaimType = ClaimTypes.Role,
+                    ClaimValue = "villains.keiththor.com/user"
+                }
+            };
+        }
+    }
+}
